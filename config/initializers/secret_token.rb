0116eca30358cdd9c65f4173e760df73a0052e7c
Christopher Fernández <fernandez.chl@gmail.com>
2013-10-23 14:31:36 -0300

    Fixeando algunos settings.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = 'e1bb59e5fcff632b4ff49eef19010e46f20f800298df5bbd5dfbddd3ac7a4cb2d8c90effc22cd45062f951506caa99040645ea918a68d6cff4ee8f1ddc8ba3f9'
+Web::Application.config.secret_token = '63305e492417e4a4d4f67cf21410d324d2720b3c1e68556ab168cad78a99728131240cf4cfa0b8d33609d0813f877963979c882dc685b3a9dd6b000d771481fe'
