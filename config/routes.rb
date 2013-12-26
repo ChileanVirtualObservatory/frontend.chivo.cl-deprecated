dba88b7480498703684580df4e29f0932bc4e397
jantogni <jantogni@csrg.cl>
2013-12-26 14:08:57 -0300

    adding workshop route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Web::Application.routes.draw do
   root to: 'home#index'
   get 'about', to: 'home#about'
   get 'institutions', to: 'home#institutions'
+  get 'workshop-program', to: 'home#workshop-program'
   
   match 'query/conesearch' => 'query#conesearch', via: [:get, :post]
   match 'query/imagesearch' => 'query#imagesearch', via: [:get, :post]
