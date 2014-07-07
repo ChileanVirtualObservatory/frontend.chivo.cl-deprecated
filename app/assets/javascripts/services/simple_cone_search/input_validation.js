14c6896d202641edd4522664f6a4a92c8d01edea
Jose del Rio <jose.delrio@alumnos.usm.cl>
2014-07-07 19:30:16 -0400

    Added the javascript code for the input validation for the Cone Search fields and for the multiselect option

---
+++ app/assets/javascripts/services/simple_cone_search/input_validation.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {	
+	/**/
+	var typewatch = (function(){
+ 		var timer = 0;
+  		return function(callback, ms){
+    	clearTimeout(timer);
+    	timer = setTimeout(callback, ms);
+  		};  
+	})();
+	/**/
+	function validateQueryFormat(target,re)
+	{
+		target.closest(".container-input").removeClass('has-error');
+		if(!re.test(target.val()) && target.val().length != 0)
+ 		{
+ 		target.closest(".container-input").addClass('has-error');
+ 		}
+	} 
+ 	$("#ra").on("keyup",function (event) {
+ 		$this = $(this);
+ 		typewatch(function () {
+ 		var re=	new RegExp(/^((\+|-)?([0-9]+)(((:[0-5][0-9]:[0-5][0-9]((.([0-9]+))?))|(.([0-9]+)))?))$/);
+ 		validateQueryFormat($this,re);
+ 	}, 1000);
+ 	});
+ 	$("#dec").on("keyup",function (event) {
+ 		$this = $(this);
+ 		typewatch(function () {
+ 		var re=	new RegExp(/^((\+|-)?([0-9]+)(((:[0-5][0-9]:[0-5][0-9]((.([0-9]+))?))|(.([0-9]+)))?))$/);
+ 		validateQueryFormat($this,re);
+ 	}, 1000);
+ 	});
+ 	$("#search_radius").on("keyup",function (event) {
+ 		$this = $(this);
+ 		typewatch(function () {
+ 		var re=	new RegExp(/^([0-9]+)(((.([0-9]+))|(:[0-5][0-9]:[0-5][0-9]((.([0-9]+))?)))?)$/);
+ 		validateQueryFormat($this,re);
+ 	}, 1000);
+ 	});
+ 		
+ 	
+
+});
