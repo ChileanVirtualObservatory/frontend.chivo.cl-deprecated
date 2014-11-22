31078d882302113dcd82bb0c7a54083e3082b66f
n3ggro <n3ggro@gmail.com>
2014-11-22 16:02:33 -0300

    SCS: updated to new controler

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -1,77 +1,177 @@
 class QueryController < ApplicationController
 	respond_to :html, :json, :js
 	
-	def conesearch
-		if params[:commit] == "sesame_search"
-			sesame_response = Array.new # => []
+	# def conesearch
+	# 	if params[:commit] == "sesame_search"
+	# 		sesame_response = Array.new # => []
 
-			sesame = params[:source_name_sesame].split(" ").join("+")
-			sesame_response << RestClient.get("http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-olfx/NSV?" + sesame)
+	# 		sesame = params[:source_name_sesame].split(" ").join("+")
+	# 		sesame_response << RestClient.get("http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-olfx/NSV?" + sesame)
 
-			xml_response = Nokogiri.XML(sesame_response[0])
-			ra = xml_response.xpath("//jradeg").children.text
-			dec = xml_response.xpath("//jdedeg").children.text
+	# 		xml_response = Nokogiri.XML(sesame_response[0])
+	# 		ra = xml_response.xpath("//jradeg").children.text
+	# 		dec = xml_response.xpath("//jdedeg").children.text
 
-			@ra = ra
-			@dec= dec
+	# 		@ra = ra
+	# 		@dec= dec
 			
-			if ra.empty? || dec.empty?
-				@ra = "ERROR"
-				@dec= "ERROR"           
-			end
+	# 		if ra.empty? || dec.empty?
+	# 			@ra = "ERROR"
+	# 			@dec= "ERROR"           
+	# 		end
 
-			respond_to do |format|
-				format.js { render 'query/sesame_resolver'}
-			end
+	# 		respond_to do |format|
+	# 			format.js { render 'query/sesame_resolver'}
+	# 		end
 
-		elsif params[:commit] == "process_position_query_list"
-			responses_dic = Hash.new
+	# 	elsif params[:commit] == "process_position_query_list"
+	# 		responses_dic = Hash.new
 			
-			params.delete("utf8")
-			params.delete("commit")
+	# 		params.delete("utf8")
+	# 		params.delete("commit")
 			
-			last_source_name_sesame = String.new
-			last_ra = String.new
-			last_dec = String.new
-			last_search_radius = String.new
+	# 		last_source_name_sesame = String.new
+	# 		last_ra = String.new
+	# 		last_dec = String.new
+	# 		last_search_radius = String.new
 
-			params.each_with_index {|(key, value), index|
-				case index%5
-				when 0 # => source name sesame          
-					last_source_name_sesame = value
-				when 1 # => ra          
-					last_ra = value
-				when 2 # => dec          
-					last_dec = value        
-				when 3 # => search_radius          
-					last_search_radius = value
-				when 4 # => resource          
+	# 		params.each_with_index {|(key, value), index|
+	# 			case index%5
+	# 			when 0 # => source name sesame          
+	# 				last_source_name_sesame = value
+	# 			when 1 # => ra          
+	# 				last_ra = value
+	# 			when 2 # => dec          
+	# 				last_dec = value        
+	# 			when 3 # => search_radius          
+	# 				last_search_radius = value
+	# 			when 4 # => resource          
 					
-					value.each {|resource|
-						short_name = resource.split("_")[0]
-						access_url = resource.split("_")[1]    
-						sr = last_search_radius.split(":")[0].to_f + last_search_radius.split(":")[1].to_f/60 + last_search_radius.split(":")[2].to_f/3600
-						response = RestClient.get(access_url + "RA=" + last_ra + "&DEC=" + last_dec + "&SR=" + sr.to_s)
-						if responses_dic[short_name] == nil 
-							responses_dic[short_name] = [response]
-						else
-							responses_dic[short_name] << response
-						end
-					}
-				end # => end case
-			}
+	# 				value.each {|resource|
+	# 					short_name = resource.split("_")[0]
+	# 					access_url = resource.split("_")[1]    
+	# 					sr = last_search_radius.split(":")[0].to_f + last_search_radius.split(":")[1].to_f/60 + last_search_radius.split(":")[2].to_f/3600
+	# 					response = RestClient.get(access_url + "RA=" + last_ra + "&DEC=" + last_dec + "&SR=" + sr.to_s)
+	# 					if responses_dic[short_name] == nil 
+	# 						responses_dic[short_name] = [response]
+	# 					else
+	# 						responses_dic[short_name] << response
+	# 					end
+	# 				}
+	# 			end # => end case
+	# 		}
 
-			responses_dic.each {|key, value|
-				responses_dic[key] = gotVotable(value)
-			}
-			@votables = responses_dic
+	# 		responses_dic.each {|key, value|
+	# 			responses_dic[key] = gotVotable(value)
+	# 		}
+	# 		@votables = responses_dic
 
-			respond_to do |format|
-				format.js { render 'query/simple_cone_search/result_tab' }
-			end
-		end # => end process_position_query_list
-	end
+	# 		respond_to do |format|
+	# 			format.js { render 'query/simple_cone_search/result_tab' }
+	# 		end
+	# 	end # => end process_position_query_list
+	# end
+	def conesearch
+    
+    if params[:commit] == "sesame_search"
+      sesame_response = Array.new # => []
 
+      sesame = params[:source_name_sesame].split(" ").join("+")
+      sesame_response << RestClient.get("http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-olfx/NSV?" + sesame)
+
+      xml_response = Nokogiri.XML(sesame_response[0])
+      ra = xml_response.xpath("//jradeg").children.text
+      dec = xml_response.xpath("//jdedeg").children.text
+
+      @ra = ra
+      @dec= dec
+      
+      if ra.empty? || dec.empty?
+        @ra = "ERROR"
+        @dec= "ERROR"           
+      end
+
+      respond_to do |format|
+        format.js { render 'query/sesame_resolver'}
+      end
+
+    elsif params[:commit] == "Add Query"
+
+      query_url = ""
+
+      params.delete("utf8")
+      params.delete("commit")
+      params.delete("controller")
+      params.delete("action")
+      params.delete("query")
+      params.delete("source_name_sesame")
+
+      raw_sources = RestClient.get('http://dachs.lirae.cl:80/external/scs')
+      sources_url = raw_sources.scan(/"accessurl": "(.*?)"/)
+      sources_name = raw_sources.scan(/"title": "(.*?)"/)
+
+      p sources_url.length
+      p sources_name
+
+      @scs_sources = Hash[sources_name.zip sources_url]
+
+      url_params = "RA=#{params[:ra]}&DEC=#{params[:dec]}&SR=#{params[:sr]}"
+      @url_params = url_params
+      @params = params  
+
+      respond_to do |format|
+        format.js { render 'query/simple_cone_search/add_query' }
+      end
+
+    # end Add Query
+    elsif params[:commit] == "Process"
+
+      params.delete("utf8")
+      params.delete("commit")
+      params.delete("action")
+
+      responses_dic = Hash.new
+
+      url_params = ""
+      query_source_url = ""
+      query_source_name = ""
+
+      params.each_with_index {|(key, value), index|
+
+        if index%2 == 0
+          url_params = value
+          next
+        end
+
+        value.each do | source | 
+          query_source_name = source.split("*@*")[0]
+          query_source_url = source.split("*@*")[1]
+          response = RestClient.get(query_source_url+url_params)
+          if responses_dic[query_source_name] == nil 
+            responses_dic[query_source_name] = [response]
+          else
+            responses_dic[query_source_name] << response
+          end
+        end
+      }
+
+      responses_dic.each do |key, value|
+        responses_dic[key] = gotVotable(value)
+      end
+
+      @votables = responses_dic
+
+      respond_to do |format|
+        format.js { render 'query/simple_cone_search/results_panel' }
+      end
+
+    # end Process
+    else
+      respond_to do |format|
+        format.html
+      end
+    end
+  end
 
 	def imagesearch
 		
@@ -151,16 +251,7 @@ class QueryController < ApplicationController
 			params.delete("controller")
 			params.delete("action")
 			params.delete("query")
-
-			if params[:ra].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:ra].to_f() < 0 || params[:ra].to_f() > 360
-				@errors << "ra"
-			end
-			if params[:dec].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:dec].to_f() < -90 || params[:dec].to_f() > 90
-				@errors << "dec"
-			end
-			if params[:size].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:size].to_f() < 0 || params[:size].to_f() > 8.5
-				@errors << "size"
-			end
+      params.delete("source_name_sesame")
 
 			if @errors == []
 
@@ -287,16 +378,7 @@ class QueryController < ApplicationController
 			params.delete("controller")
 			params.delete("action")
 			params.delete("query")
-
-			if params[:ra].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:ra].to_f() < 0 || params[:ra].to_f() > 360
-				@errors << "ra"
-			end
-			if params[:dec].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:dec].to_f() < -90 || params[:dec].to_f() > 90
-				@errors << "dec"
-			end
-			if params[:size].match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil || params[:size].to_f() < 0 || params[:size].to_f() > 8.5
-				@errors << "size"
-			end
+      params.delete("source_name_sesame")
 
 			if @errors == []
 
