d08df697b076ffd7ab4a4fbf0599920df6299200
vansimonsen <vansimonsen@gmail.com>
2015-03-19 17:57:46 -0300

    comentadas rutas de vistas no utilizadas

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,20 @@ Web::Application.routes.draw do
   root to: 'home#index'
   get 'about', to: 'home#about'
   get 'institutions', to: 'home#institutions'
-  get 'workshop-program', to: 'home#workshop-program'
+  #get 'workshop-program', to: 'home#workshop-program'
   
   match 'query/conesearch' => 'query#conesearch', via: [:get, :post]
   match 'query/imagesearch' => 'query#imagesearch', via: [:get, :post]
   match 'query/spectralsearch' => 'query#spectralsearch', via: [:get, :post]
-  match 'query/tablesearch' => 'query#tablesearch', via: [:get, :post]
-  match 'query/advancesearch' => 'query#advancesearch', via: [:get, :post]
-  match 'query/all_searches' => 'query#all_searches', via: [:get, :post]
+  #match 'query/tablesearch' => 'query#tablesearch', via: [:get, :post]
+  #match 'query/advancesearch' => 'query#advancesearch', via: [:get, :post]
+  #match 'query/all_searches' => 'query#all_searches', via: [:get, :post]
 
-  match 'form_option/option1' => 'form_option#option1', via: [:get]
-  match 'form_option/option2' => 'form_option#option2', via: [:get]
-  match 'form_option/option3' => 'form_option#option3', via: [:get]
-  match 'form_option/alma' => 'form_option#alma', via: [:get]
-  match 'form_option/alma2' => 'form_option#alma2', via: [:get]
+  #match 'form_option/option1' => 'form_option#option1', via: [:get]
+  #match 'form_option/option2' => 'form_option#option2', via: [:get]
+  #match 'form_option/option3' => 'form_option#option3', via: [:get]
+  #match 'form_option/alma' => 'form_option#alma', via: [:get]
+  #match 'form_option/alma2' => 'form_option#alma2', via: [:get]
 
   match 'all_news' => 'news#all_news', via: [:get]
   match 'undergraduate_thesis' => 'news#undergraduate_thesis', via: [:get]
